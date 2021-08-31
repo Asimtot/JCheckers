24ce24a1db97b2b6802c42f586efba211d1325e8
Efe Can Tepe <can.tepe@ug.bilkent.edu.tr>
2021-09-01 01:45:28 +0300

    Some other important classes

---
+++ src/main/java/gui/ClockPanel.java
@@ -0,0 +1,37 @@
+package gui;
+
+import piece.Alliance;
+import timer.Clock;
+import timer.TimeControl;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ClockPanel extends JPanel {
+
+    private JLabel timeLabel;
+    private Clock clock;
+
+    public ClockPanel(Alliance alliance, TimeControl timeControl){
+        clock = new Clock(alliance, new TimeControl(0,5,5,2), this);
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Serif",Font.PLAIN, 50));
+        timeLabel.setBorder(BorderFactory.createBevelBorder(1));
+
+        System.out.println("Clock Panel constructor");
+        add(timeLabel);
+        updateTimeLabel();
+    }
+
+    public void updateTimeLabel(){
+        String secondsString = String.format("%02d", clock.getSeconds());
+        String minutesString = String.format("%02d", clock.getMinutes());
+        String hoursString = String.format("%02d", clock.getHours());
+
+        timeLabel.setText(hoursString + ":" + minutesString + ":" + secondsString );
+    }
+
+    public Clock getClock(){
+        return clock;
+    }
+}
